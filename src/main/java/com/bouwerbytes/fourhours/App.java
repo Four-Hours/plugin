48fab8e6d73e39f8507db50b1afb21ef595bb9b0
Dylan Parks <dylan@bouwerbytes.com>
2021-11-22 16:22:59 -0600

    Cleaned Imports Created skeleton db connection

--- src/main/java/com/bouwerbytes/fourhours/App.java
+++ src/main/java/com/bouwerbytes/fourhours/App.java
@@ -1,12 +1,8 @@
 package com.bouwerbytes.fourhours;
 
-import java.io.File;
-import java.io.IOException;
 import java.util.logging.Logger;
 
-import org.bukkit.configuration.InvalidConfigurationException;
 import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class App extends JavaPlugin {
@@ -14,12 +10,11 @@ public class App extends JavaPlugin {
     private Logger logger;
 
     // Constructor
-    // public App() {
-    //     configFile = getConfig();
-    //     logger = getLogger();
-    // }
+    public App() {
+        configFile = getConfig();
+        logger = getLogger();
+    }
 
-    @Override
     public void onEnable() {
         getLogger().info("Hello, SpigotMC!");
         saveDefaultConfig();
--- src/main/java/com/bouwerbytes/fourhours/Database.java
+++ src/main/java/com/bouwerbytes/fourhours/Database.java
@@ -5,20 +5,29 @@ import java.util.logging.Logger;
 
 import org.bukkit.configuration.file.FileConfiguration;
 
+// TODO Figure out how connecting and var storage works
 public class Database {
 
     private Connection conn;
     private FileConfiguration configFile;
     private Logger logger;
+    private Statement stmt;
 
-    // public Database() {
-    //     App app = new App();
-    //     configFile = app.getConfig();
-    //     logger = app.getLogger();
-    // }
+    public Database() {
+        App app = new App();
+        configFile = app.getConfig();
+        logger = app.getLogger();
+    }
     
-    public void Connect() throws SQLException {
+    //TODO Actually connect lmao
+    public void Connect(String host, int port, String user, String pass) throws SQLException {
         //create connection for a server installed in localhost, with a user "root" with no password
         //conn = DriverManager.getConnection("jdbc:mariadb://localhost/", "root", null); 
+    }
+
+    // TODO Actually make this query the connection
+    public ResultSet Query(String query) {
+        ResultSet temp = null;
+        return temp;
     }
 }
